46c479d907e54edaa2470594e9585a2c3b6cc6a4
olena-nikitchenko <helnikitka@gmail.com>
2023-02-05 13:51:36 +0200

    add task with Repeta

---
+++ js/autochek task-5/task with Repeta.js
@@ -0,0 +1,18 @@
+const counter = {
+    value: 0,
+    increment(value) {
+        console.log('increment → this', this);
+        this.value += value;
+    },
+    decrement(value) {
+        console.log('decrement → this', this);
+        this.value += value;
+    },
+};
+const updateCounter = function (value, operation) {
+    operation(value);
+};
+
+updateCounter(10, counter.increment);
+
+updateCounter(5, counter.decrement);
